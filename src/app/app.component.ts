4d3f578163417fe7b6d0db38a88fb5f9eb6db947
Adrian Carriger <mail@adrianandnicole.com>
2016-08-20 17:44:52 -0700

    Added navbar, fixed footer height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { NavbarComponent } from './shared/index';
@@ -7,7 +7,8 @@ import { NavbarComponent } from './shared/index';
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'app works!';
