759d5545c79f199130bd4248b482f4e87d021420
Adrian Carriger <mail@adrianandnicole.com>
2016-09-16 20:41:04 -0700

    chore: update Angular to 2.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent { }
